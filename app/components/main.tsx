07f15a1bd3b80e9d4d99fa56032207420f5381cb
Muskan492 <irshadalilashari5419@gmail.com>
2025-01-29 03:05:17 -0800

    Add files via upload

---
+++ app/components/main.tsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+    return (
+<header>
+      <ul>
+        <li>
+          <a href="#">Home</a> <br />
+          <a href="#">Contact-us</a> <br />
+          <a href="#">About</a>
+        </li>
+      </ul>
+    
+  </header>
+
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+
+import Header from "./components/main";
+
+
+
+export default function Home() {
+  return (
+  <div>
+    <Header/>
+  
+  </div>
+  );
+}
